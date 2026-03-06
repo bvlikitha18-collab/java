71d0dc7b6a2dc28a6b8d1ac752f5542824fb47d2
Likitha <bvlikitha18@gmail.com>
2026-03-06 15:02:48 +0530

    New file

---
+++ liki.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Defining variables with set values
+        int firstNumber = 10;
+        int secondNumber = 20;
+
+        // Performing the calculation
+        int sum = firstNumber + secondNumber;
+
+        // Displaying the output
+        System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is: " + sum);
+    }
+}
